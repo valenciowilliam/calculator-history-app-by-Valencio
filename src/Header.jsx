db3bdfeb956ab1bf2e0ad31d20d27d84d0a1e3e4
Valencio William <valenciowilliam8@gmail.com>
2026-01-31 09:37:18 +0530

    R

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ name }) => {
   return (
     <header className="site-header">
       <div className="brand">
-        <h1 className="title">A Scientific Calculatorr</h1>
+        <h1 className="title">A Scientific Calculator</h1>
         <p className="subtitle">Made by {name}</p>
       </div>
     </header>
